48ffec60e0ada43333f1d9c4aa2f5769d9d72a50
Oreus-F <fourniol.lucas@gmail.com>
2025-12-15 03:38:07 +0100

    transfer old script file js into the right folder

---
+++ script.js
@@ -0,0 +1,60 @@
+class LinkedList{
+    constructor(){
+        this.head = null
+    }
+
+
+    
+    append(value, node = this.head){
+
+        if(node === this.head && node === null){
+            this.head = new Node(value)
+            return
+        }
+
+        if(node.nextNode === null){
+            node.nextNode = new Node(value)
+            
+        } else {
+            let temp = node.nextNode;
+            return this.append(value, temp)
+        }
+
+    }
+
+
+    printTest(){
+
+        if(!this.head){
+            return;
+        }
+
+        let node = this.head;
+        let str = "";
+
+        while(node){
+            str += node.value + ' ';
+            node = node.nextNode;
+        }
+
+        return str
+    }
+}
+
+class Node{
+    constructor(value = null, next = null){
+        this.value = value,
+        this.nextNode = next
+    }
+}
+
+
+const list = new LinkedList;
+list.append("dog");
+list.append("cat");
+list.append("bird");
+list.append("train");
+
+console.log(list.printTest())
+
+list
